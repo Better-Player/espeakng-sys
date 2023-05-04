ebb8b10bc552b0e1c1ac85d3e6b2bf80fd5bef1e
Andrew Gazelka <andrew.gazelka@gmail.com>
2023-05-04 08:22:52 -0500

    add static linking

--- build.rs
+++ build.rs
@@ -3,8 +3,42 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
+static STATIC_LIBRARIES: &[&str] = &[
+    "stdc++",
+    "sonic",
+    "pthread",
+    "pcaudio",
+    "asound",
+    "espeak-ng",
+];
+
+fn add_search_paths() {
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    match target_arch.as_str() {
+        "aarch64" => {
+            println!("cargo:rustc-link-search=native=/usr/lib/aarch64-linux-gnu");
+            println!("cargo:rustc-link-search=native=/usr/lib/gcc/aarch64-linux-gnu/11");
+        }
+        "x86" | "x86_64" => {
+            println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+            println!("cargo:rustc-link-search=native=/usr/lib/gcc/x86_64-linux-gnu/11");
+        }
+        _ => panic!("Unsupported architecture: {}", target_arch),
+    }
+    println!("cargo:rustc-link-search=native=/usr/lib");
+    println!("cargo:rustc-link-search=native=/usr/local/lib");
+}
+
 fn main() {
-    println!("cargo:rustc-link-lib=espeak-ng");
+    if cfg!(feature = "static") {
+        add_search_paths();
+        for lib in STATIC_LIBRARIES {
+            println!("cargo:rustc-link-lib=static={lib}");
+        }
+    } else {
+        println!("cargo:rustc-link-lib=espeak-ng");
+    }
+
     println!("cargo:rerun-if-changed=headers/wrapper.h");
 
     let bindings = bindgen::Builder::default()
