a55eb4598c2b9d3d91dccbc074345e763cd8896e
Andrew Gazelka <andrew.gazelka@gmail.com>
2023-06-16 05:37:34 -0500

    update static linking script

--- build.rs
+++ build.rs
@@ -2,44 +2,56 @@ extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
-static STATIC_LIBRARIES: &[&str] = &[
-    "stdc++",
-    "sonic",
-    // "pthread",
-    "pcaudio",
-    "asound",
-    // "libc",
-    "espeak-ng",
-];
+fn build_static_espeak_ng() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let espeak_dir = format!("{}/espeak-ng", out_dir);
+    let espeak_build_dir = format!("{}/build", &espeak_dir);
+    let espeak_lib_dir = format!("{}/lib", &espeak_build_dir);
 
-fn add_arch(arch: &str) {
-    println!("cargo:rustc-link-search=native=/usr/lib/{arch}");
-    println!("cargo:rustc-link-search=native=/usr/lib/gcc/{arch}/10");
-    println!("cargo:rustc-link-search=native=/usr/lib/gcc/{arch}/11");
-}
+    Command::new("git")
+        .args([
+            "clone",
+            "https://github.com/espeak-ng/espeak-ng",
+            &espeak_dir,
+        ])
+        .status()
+        .expect("Failed to clone espeak-ng");
+    Command::new("./autogen.sh")
+        .current_dir(&espeak_dir)
+        .status()
+        .expect("Failed to run autogen for espeak-ng");
+    Command::new("./configure")
+        .args([
+            "--prefix",
+            &espeak_build_dir,
+            "--without-sonic",
+            "--without-pcaudiolib",
+            "--without-speechplayer",
+            "--enable-static",
+            "--disable-shared",
+        ])
+        .current_dir(&espeak_dir)
+        .status()
+        .expect("Failed to configure espeak-ng");
+    Command::new("make")
+        .current_dir(&espeak_dir)
+        .status()
+        .expect("Failed to build espeak-ng");
+    Command::new("make")
+        .args(&["install"])
+        .current_dir(&espeak_dir)
+        .status()
+        .expect("Failed to install espeak-ng");
 
-fn add_search_paths() {
-    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    match target_arch.as_str() {
-        "aarch64" => {
-            add_arch("aarch64-linux-gnu");
-        }
-        "x86" | "x86_64" => {
-            add_arch("x86_64-linux-gnu");
-        }
-        _ => panic!("Unsupported architecture: {}", target_arch),
-    }
-    println!("cargo:rustc-link-search=native=/usr/lib");
-    println!("cargo:rustc-link-search=native=/usr/local/lib");
+    println!("cargo:rustc-link-search=native={}", espeak_lib_dir);
+    println!("cargo:rustc-link-lib=static=espeak-ng");
 }
 
 fn main() {
     if cfg!(feature = "static") {
-        add_search_paths();
-        for lib in STATIC_LIBRARIES {
-            println!("cargo:rustc-link-lib=static={lib}");
-        }
+        build_static_espeak_ng();
     } else {
         println!("cargo:rustc-link-lib=espeak-ng");
     }
